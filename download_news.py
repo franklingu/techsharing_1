b21b5b6e40c01addffbae916cb790882ef9ab0a9
franklingu <jgu@dytechlab.com>
2017-05-16 20:17:24 +0800

    add to download worker

--- download_news.py
+++ download_news.py
@@ -75,6 +75,44 @@ class ThreadDownloader(NewsDownloader):
         for link in links:
             task_queue.put(link)
         for idx in range(self.num_threads):
+            worker = multiprocessing.Process(
+                target=self._download_worker, args=(task_queue, result_queue)
+            )
+            worker.start()
+        task_queue.join()
+        result = {}
+        while True:
+            try:
+                link, res_content = result_queue.get(timeout=0.1)
+                result[link] = res_content
+            except queue.Empty:
+                return result
+
+    def _download_worker(self, task_queue, result_queue):
+        while True:
+            try:
+                link = task_queue.get(timeout=0.1)
+                res_content = self._send_news_request(link)
+                result_queue.put((link, res_content))
+                task_queue.task_done()
+            except queue.Empty:
+                return
+
+
+class ProcessDownloader(NewsDownloader):
+    """docstring for ProcessDownloader"""
+    def __init__(self, *arg, **kwargs):
+        super(ProcessDownloader, self).__init__()
+        self.num_processes = 4
+        if 'num_processes' in kwargs:
+            self.num_processes = kwargs['num_processes']
+
+    def download_news(self, links):
+        task_queue = multiprocessing.JoinableQueue()
+        result_queue = multiprocessing.Queue()
+        for link in links:
+            task_queue.put(link)
+        for idx in range(self.num_threads):
             worker = threading.Thread(
                 target=self._download_worker, args=(task_queue, result_queue)
             )
@@ -94,16 +132,9 @@ class ThreadDownloader(NewsDownloader):
                 link = task_queue.get(timeout=0.1)
                 res_content = self._send_news_request(link)
                 result_queue.put((link, res_content))
-                task_queue.done()
+                task_queue.task_done()
             except queue.Empty:
                 return
-
-
-class ProcessDownloader(NewsDownloader):
-    """docstring for ProcessDownloader"""
-    def __init__(self, *arg, **kwargs):
-        super(ProcessDownloader, self).__init__()
-        self.arg = arg
 
 
 class ConcurrentDownloader(NewsDownloader):
